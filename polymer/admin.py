77a34621e24326d39f8ae2970ba96fbd37c84087
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-27 23:19:29 -0400

    update admin site

--- polymer/admin.py
+++ polymer/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from polymer.models import Day, Task
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('date', 'account')
+    inlines = [TaskInline]
+
+admin.site.register(Day, DayAdmin)
